95d3cb0f9f507182dde6e8c2a275f00cb2b6efda
Cristian Suarez <casm2203@email.com>
2023-09-29 12:09:36 -0500

    Validaciones

--- src/app/dashboard/favorites/page.jsx
+++ src/app/dashboard/favorites/page.jsx
@@ -1,64 +1,89 @@
-"use client"
+"use client";
 import { Backdrop, Box, CircularProgress, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { getSession } from "next-auth/react";
 import Book from "@/components/Book";
 
 const Favorites = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [filtredBooks, setFiltredBooks] = useState([]);
 
-    const [isLoading, setIsLoading] = useState(false)
-    const [books, setBooks] = useState([])
-    const [filtredBooks, setFiltredBooks] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      const session = await getSession();
+      const user = session.user.id;
+      let favorites = JSON.parse(localStorage.getItem("favorites"));
+      if (favorites && favorites[user]) {
+        const res = await fetch(`/api/books`);
+        const dataRes = await res.json();
+        setBooks(
+          dataRes.result.filter((book) => favorites[user].includes(book.id))
+        );
+        setIsLoading(false);
+      }
+    };
+    setIsLoading(true);
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const session = await getSession()
-            const user = session.user.id
-            let favorites = JSON.parse(localStorage.getItem("favorites"))
-            if (favorites && favorites[user]) {
-                const res = await fetch(`/api/books`);
-                const dataRes = await res.json();
-                setBooks(dataRes.result.filter(book => favorites[user].includes(book.id)))
-                setIsLoading(false)
-            }
-        }
-        setIsLoading(true)
-        fetchData()
-    }, [])
+  const updateBooksList = (id) => {
+    setBooks((prev) => prev.filter((book) => book.id != id));
+  };
 
-    const updateBooksList = (id) => {
-        setBooks(prev => prev.filter(book => book.id != id))
-    }
-
+  if (books && books.length === 0) {
     return (
-        <>
-            <Box sx={{
-                display: 'flex',
-                gap: 3,
-                flexWrap: 'wrap',
-                justifyContent: 'center'
-            }}>
-                {
-                    books?.length
-                        ? books.map((book) => (
-                            <Book
-                                key={book.id}
-                                data={book}
-                                isFavoriteList
-                                onRemoveFavoriteList={updateBooksList}
-                            />
-                        ))
-                        : <Typography color="secondary.main" mt={4}>No hay favoritos.</Typography>
-                }
-            </Box>
-            <Backdrop
-                sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer - 1 }}
-                open={isLoading}
-            >
-                <CircularProgress color="inherit" />
-            </Backdrop>
-        </>
+      <Box sx={{ textAlign: "center" }}>
+        <img
+          src={"/images/Empty.svg"}
+          alt="empty"
+          style={{ width: "50%", maxWidth: "450px" }}
+        />
+        <Typography
+          variant="h5"
+          color="secondary.main"
+          pt={4}
+          textAlign="center"
+        >
+          No hay libros en tu lista de favoritos.
+        </Typography>
+      </Box>
     );
-}
+  }
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          gap: 3,
+          flexWrap: "wrap",
+          justifyContent: "center",
+        }}
+      >
+        {books?.length ? (
+          books.map((book) => (
+            <Book
+              key={book.id}
+              data={book}
+              isFavoriteList
+              onRemoveFavoriteList={updateBooksList}
+            />
+          ))
+        ) : (
+          <Typography color="secondary.main" mt={4}>
+            No hay favoritos.
+          </Typography>
+        )}
+      </Box>
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer - 1 }}
+        open={isLoading}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
+};
 
 export default Favorites;
